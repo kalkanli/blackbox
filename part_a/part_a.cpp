c0782f1b568cb82b1e6b0aabfe0e2a1589dd9015
tahakalkanli <abdullahtahakalkanli@gmail.com>
2021-12-10 15:10:51 +0300

    part_a final final

--- part_a/part_a.cpp
+++ part_a/part_a.cpp
@@ -16,50 +16,49 @@ int main(int argc, char *argv[])
     pipe(&fd1[0]);
     pipe(&fd2[0]);
 
-    char output[100] = { '\0' };
+    char output[100] = {'\0'};
 
-    int pid = fork();
-    if (pid > 0) // parent process
-    {
-        close(fd1[0]);
-        close(fd2[1]);
-
-        string input;
-        getline (cin, input);
-        write(fd1[1], input.c_str(), input.length());
-        close(fd1[1]);
-
-        int file = open(output_file_name, O_WRONLY | O_CREAT | O_APPEND, 0777);
-        dup2(file, STDOUT_FILENO);
-        
-        read(fd2[0], output, sizeof(output));
-        close(fd2[0]);
-    }
-    else // child process
+    int pid;
+    if ((pid = fork()) == 0) // child process
     {
         close(fd1[1]);
         close(fd2[0]);
-        
+
         dup2(fd1[0], STDIN_FILENO);
-        
+
         dup2(fd2[1], STDERR_FILENO);
         dup2(fd2[1], STDOUT_FILENO);
-        
+
         execlp(blackbox, "blackbox", NULL);
     }
 
+    close(fd1[0]);
+    close(fd2[1]);
+
+    string number1;
+    string number2;
+    getline(cin, number1);
+    getline(cin, number2);
+    string input = number1 + " " + number2;
+    write(fd1[1], input.c_str(), input.length());
+    close(fd1[1]);
+
+    int file = open(output_file_name, O_WRONLY | O_CREAT | O_APPEND, 0777);
+    dup2(file, STDOUT_FILENO);
+
+    read(fd2[0], output, sizeof(output));
+    close(fd2[0]);
 
     int status;
     waitpid(pid, &status, 0);
-    if(status == 0) {
+    if (status == 0)
+    {
         printf("SUCCESS:\n");
-    } 
+    }
     else
     {
         printf("FAIL:\n");
     }
 
     printf("%s", output);
-
-    
 }
