a019b493d4e86bceb54f313d067c2814f073dee3
tahakalkanli <abdullahtahakalkanli@gmail.com>
2021-12-10 14:58:26 +0300

    documentation needed

--- part_a/part_a.cpp
+++ part_a/part_a.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fcntl.h>
 #include <cstring>
+#include <sys/wait.h>
 
 using namespace std;
 
@@ -10,29 +11,55 @@ int main(int argc, char *argv[])
     char *blackbox = argv[1];
     char *output_file_name = argv[2];
 
-    int fd[2];
-    pipe(&fd[0]);
+    int fd1[2];
+    int fd2[2];
+    pipe(&fd1[0]);
+    pipe(&fd2[0]);
+
+    char output[100] = { '\0' };
 
     int pid = fork();
     if (pid > 0) // parent process
     {
-        close(fd[0]);
+        close(fd1[0]);
+        close(fd2[1]);
 
         string input;
         getline (cin, input);
-        write(fd[1], input.c_str(), input.length());
+        write(fd1[1], input.c_str(), input.length());
+        close(fd1[1]);
+
+        int file = open(output_file_name, O_WRONLY | O_CREAT | O_APPEND, 0777);
+        dup2(file, STDOUT_FILENO);
+        
+        read(fd2[0], output, sizeof(output));
+        close(fd2[0]);
     }
     else // child process
     {
-        close(fd[1]);
-
-        int file = open(output_file_name, O_WRONLY | O_CREAT | O_APPEND, 0777);
+        close(fd1[1]);
+        close(fd2[0]);
         
-        dup2(fd[0], STDIN_FILENO);
-        dup2(file, STDERR_FILENO);
-        dup2(file, STDOUT_FILENO);
-
-
-        int result = execlp(blackbox, "blackbox", NULL);
+        dup2(fd1[0], STDIN_FILENO);
+        
+        dup2(fd2[1], STDERR_FILENO);
+        dup2(fd2[1], STDOUT_FILENO);
+        
+        execlp(blackbox, "blackbox", NULL);
     }
+
+
+    int status;
+    waitpid(pid, &status, 0);
+    if(status == 0) {
+        printf("SUCCESS:\n");
+    } 
+    else
+    {
+        printf("FAIL:\n");
+    }
+
+    printf("%s", output);
+
+    
 }
